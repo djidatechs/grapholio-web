af060385410d13bd48bd21fca03ad2a25691a9fd
issamDjida <i.djida@esi-sba.dz>
2026-02-16 21:01:53 +0100

    x

--- src/Pages/Application/Dashboard/Operations/CodeOperations.jsx
+++ src/Pages/Application/Dashboard/Operations/CodeOperations.jsx
@@ -1,175 +1,175 @@
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 
-import {  Controlled as CodeMirror } from 'react-codemirror2';
+import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/theme/dracula.css';
-import {useGrapholio} from "../../Context.tsx";
-import {IoMdArrowRoundBack} from "react-icons/io";
-import {BsPauseBtnFill, BsPlayFill} from "react-icons/bs";
-import {BiSolidCopy} from "react-icons/bi";
-import {OperationDash} from "../../../../Constants.ts";
+import { useGrapholio } from "../../Context.tsx";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { BsPauseBtnFill, BsPlayFill } from "react-icons/bs";
+import { BiSolidCopy } from "react-icons/bi";
+import { OperationDash } from "../../../../Constants.ts";
 //import {GrapholioCommandManager} from "../../../../Logic/GraphlolioScriptLanguage/GrapholioCommandManager.ts";
-import {GrapholioCommandManager_refactor} from "../../../../Logic/GraphlolioScriptLanguage/GCMrefactor.ts"
-import {TbCircleLetterL} from "react-icons/tb";
-import {ToastInfoWithAction} from "../../../../Logic/GrapholioManager/Messages.ts";
+import { GrapholioCommandManager_refactor } from "../../../../Logic/GraphlolioScriptLanguage/GCMrefactor.ts"
+import { TbCircleLetterL } from "react-icons/tb";
+import { ToastInfoWithAction } from "../../../../Logic/GrapholioManager/Messages.ts";
 function CodeOperations() {
-        const editor = useRef(null)
-        const wrapper = useRef(null)
-        const container = useRef(null)
-        const logContainer = useRef(null)
-        const pageContainer = useRef(null)
+    const editor = useRef(null)
+    const wrapper = useRef(null)
+    const container = useRef(null)
+    const logContainer = useRef(null)
+    const pageContainer = useRef(null)
 
-        const [currentCode , setCurrentCode ] = useState("");
-        const {operations  ,grapholioManager} = useGrapholio()
-        const [log , setLog] = useState([])
-        const [runnig , setRunning ] = useState(false);
+    const [currentCode, setCurrentCode] = useState("");
+    const { operations, grapholioManager } = useGrapholio()
+    const [log, setLog] = useState([])
+    const [runnig, setRunning] = useState(false);
 
 
-        const editorWillUnmount = () => {
-                //editor.current.display.wrapper.remove()
-        }
-        const RunGivosCode= ()=> {
-            setRunning(true);
-            fetch("http://localhost:3000/compiler", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ text: currentCode })
+    const editorWillUnmount = () => {
+        //editor.current.display.wrapper.remove()
+    }
+    const RunGivosCode = () => {
+        setRunning(true);
+        fetch("https://grapholio-web-api-qspr.onrender.com/compiler", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: currentCode })
+        })
+            .then(response => response.json())
+            .then(async (data) => {
+                let gcm = (new GrapholioCommandManager_refactor(grapholioManager, setLog))
+                if (data.data) await gcm.eval(data.data)
+                else setLog((current) => [...current || [], { content: "# " + data.error, type: "error" }])
+                setRunning(false);
             })
-                .then(response => response.json())
-                .then( async(data) => {
-                     let gcm = (new GrapholioCommandManager_refactor(grapholioManager, setLog))
-                    if (data.data) await gcm.eval(data.data)
-                    else setLog((current) => [...current||[],{content:"# "+data.error,type:"error"}])
-                    setRunning(false);
-                })
-                .catch(error => {
-                    setRunning(false);
-                    console.error('XOERROR:', error)
-                });
-        }
+            .catch(error => {
+                setRunning(false);
+                console.error('XOERROR:', error)
+            });
+    }
 
-        const handleChange = (editor, data, value) => {
-            setCurrentCode(value)
-            grapholioManager.write(value)
-        }
-        useEffect(() => {
-            ToastInfoWithAction(
-                "Click here to see code examples and tutorial in the documentation",
-                ()=>window.open("https://grapholio.djidax.com/documentation/scripting", '_blank')
-                )
-            setCurrentCode (grapholioManager.graph_script())
-            editor.current.setValue ( grapholioManager.graph_script())
-        }, []);
+    const handleChange = (editor, data, value) => {
+        setCurrentCode(value)
+        grapholioManager.write(value)
+    }
+    useEffect(() => {
+        ToastInfoWithAction(
+            "Click here to see code examples and tutorial in the documentation",
+            () => window.open("https://grapholio.djidax.com/documentation/scripting", '_blank')
+        )
+        setCurrentCode(grapholioManager.graph_script())
+        editor.current.setValue(grapholioManager.graph_script())
+    }, []);
 
-        useEffect(()=>{
-            if (container.current.style) container.current.style.height =  "44%"
-            const  oh = container.current?.clientHeight ;
-            if (oh && wrapper.current?.ref?.style )  wrapper.current.ref.style.height = oh+"px" ;
-            if (oh && editor.current?.ref?.style )  editor.current.ref.style.height = oh+"px" ;
+    useEffect(() => {
+        if (container.current.style) container.current.style.height = "44%"
+        const oh = container.current?.clientHeight;
+        if (oh && wrapper.current?.ref?.style) wrapper.current.ref.style.height = oh + "px";
+        if (oh && editor.current?.ref?.style) editor.current.ref.style.height = oh + "px";
 
-            const  log_h = logContainer.current?.offsetTop ;
-            const  page_h = pageContainer.current?.clientHeight ;
-            logContainer.current.style.height = (page_h - log_h -30) +"px"
+        const log_h = logContainer.current?.offsetTop;
+        const page_h = pageContainer.current?.clientHeight;
+        logContainer.current.style.height = (page_h - log_h - 30) + "px"
 
 
 
-            fetch("http://localhost:3000/compiler", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({text: "//nothing"})
-            }).then(() => console.log("Givos ... "))
+        fetch("https://grapholio-web-api-qspr.onrender.com/compiler", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: "//nothing" })
+        }).then(() => console.log("Givos ... "))
 
-            return ()=>editor.current.display.wrapper.remove()
+        return () => editor.current.display.wrapper.remove()
 
-        },[])
+    }, [])
 
 
-        return (
-            <div className={"p-2 h-full"}  ref={pageContainer}>
-                <div className="flex flex-wrap align-bottom " >
-                    <IoMdArrowRoundBack
-                        onClick={()=>operations.operateOn(OperationDash.INFO)}
-                        className={"flex-auto h-5 lg:h-8 w-5 lg:w-8  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"}/>
-                    {
-                        !runnig ?
+    return (
+        <div className={"p-2 h-full"} ref={pageContainer}>
+            <div className="flex flex-wrap align-bottom " >
+                <IoMdArrowRoundBack
+                    onClick={() => operations.operateOn(OperationDash.INFO)}
+                    className={"flex-auto h-5 lg:h-8 w-5 lg:w-8  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"} />
+                {
+                    !runnig ?
                         <BsPlayFill
-                        onClick={RunGivosCode}
-                        className={"flex-auto h-5 lg:h-8 w-5 lg:w-8  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"}/>
+                            onClick={RunGivosCode}
+                            className={"flex-auto h-5 lg:h-8 w-5 lg:w-8  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"} />
                         :
                         <BsPauseBtnFill
-                        onClick={RunGivosCode}
-                        className={"flex-auto h-5 lg:h-8 w-5 lg:w-8  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"}/>
-                    }
-                    <BiSolidCopy
-                        onClick={()=>navigator.clipboard.writeText(currentCode)}
-                        className={"flex-auto h-7 w-7  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"}/>
-                    <TbCircleLetterL
-                        onClick={()=>{
-                            const dis = logContainer.current.style.display
-                            container.current.style.height = (dis === "block" ||dis === "") ? "82%" : "44%"
-                            const  oh = container.current?.clientHeight ;
-                            if (oh && wrapper.current?.ref?.style )  wrapper.current.ref.style.height = oh+"px" ;
-                            if (oh && editor.current?.ref?.style )  editor.current.ref.style.height = oh+"px" ;
-                            logContainer.current.style.display = (dis === "block" ||dis === "") ? "none" : "block"
+                            onClick={RunGivosCode}
+                            className={"flex-auto h-5 lg:h-8 w-5 lg:w-8  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"} />
+                }
+                <BiSolidCopy
+                    onClick={() => navigator.clipboard.writeText(currentCode)}
+                    className={"flex-auto h-7 w-7  fill-primary hover:fill-secondary hover:scale-105 transition duration-200 cursor-pointer"} />
+                <TbCircleLetterL
+                    onClick={() => {
+                        const dis = logContainer.current.style.display
+                        container.current.style.height = (dis === "block" || dis === "") ? "82%" : "44%"
+                        const oh = container.current?.clientHeight;
+                        if (oh && wrapper.current?.ref?.style) wrapper.current.ref.style.height = oh + "px";
+                        if (oh && editor.current?.ref?.style) editor.current.ref.style.height = oh + "px";
+                        logContainer.current.style.display = (dis === "block" || dis === "") ? "none" : "block"
 
-                        }}
-                        className={"flex-auto h-7 w-7  text-primary hover:text-secondary hover:scale-105 transition duration-200 cursor-pointer"}/>
-                </div>
-                <div className="divider ">Code</div>
-                <div ref={container} className={" w-full h-[44%]  bg-[#282a36]"}>
+                    }}
+                    className={"flex-auto h-7 w-7  text-primary hover:text-secondary hover:scale-105 transition duration-200 cursor-pointer"} />
+            </div>
+            <div className="divider ">Code</div>
+            <div ref={container} className={" w-full h-[44%]  bg-[#282a36]"}>
 
-                    <CodeMirror
-                        className={" text-[16px] overflow-hidden p-0"}
-                        ref={wrapper}
-                        options={{
-                                mode: 'javascript',
-                                theme:'dracula',
-                                lineNumbers: true,
-                                scrollbarStyle: 'null',
-                        }}
+                <CodeMirror
+                    className={" text-[16px] overflow-hidden p-0"}
+                    ref={wrapper}
+                    options={{
+                        mode: 'javascript',
+                        theme: 'dracula',
+                        lineNumbers: true,
+                        scrollbarStyle: 'null',
+                    }}
 
-                        value={currentCode}
-                        onBeforeChange={handleChange}
-                        editorDidMount={(e) => {
-                            editor.current = e
-                            const  oh = container.current?.clientHeight ;
-                            if (oh && wrapper.current?.ref?.style ) wrapper.current.ref.style.height = oh + "px";
-                            if (oh && editor.current?.ref?.style )  editor.current.ref.style.height = oh+"px" ;
-                        }}
-                        editorWillUnmount={editorWillUnmount}
-                    />
-                </div>
-                <div className="divider ">LOG</div>
+                    value={currentCode}
+                    onBeforeChange={handleChange}
+                    editorDidMount={(e) => {
+                        editor.current = e
+                        const oh = container.current?.clientHeight;
+                        if (oh && wrapper.current?.ref?.style) wrapper.current.ref.style.height = oh + "px";
+                        if (oh && editor.current?.ref?.style) editor.current.ref.style.height = oh + "px";
+                    }}
+                    editorWillUnmount={editorWillUnmount}
+                />
+            </div>
+            <div className="divider ">LOG</div>
 
-                <div ref={logContainer} className={"overflow-auto scrollbar-thin scrollbar-track-emerald-950 scrollbar-thumb-primary   "}>
-                    <div className={"  font-bold break-words  "}>
+            <div ref={logContainer} className={"overflow-auto scrollbar-thin scrollbar-track-emerald-950 scrollbar-thumb-primary   "}>
+                <div className={"  font-bold break-words  "}>
                     {
-                        ! log.length ? <></>
-                        : (
-                                log.map((logItem,index) => {
+                        !log.length ? <></>
+                            : (
+                                log.map((logItem, index) => {
 
                                     return (
 
 
-                                            <pre key={"pxk"+index} data-prefix="3" >
-                                               <code
-                                                   className={
-                                                       logItem.type === "normal" && (typeof logItem.content == "string" || typeof logItem.content == "boolean"|| typeof logItem.content == "number"|| !logItem.content )
-                                                           ? "pl-1  text-emerald-500 break-words "
-                                                           : logItem.type === "warning" ? "pl-1 bg-warning text-warning-content py-1  "
-                                                           : "pl-1 bg-error text-error-content py-1  "
+                                        <pre key={"pxk" + index} data-prefix="3" >
+                                            <code
+                                                className={
+                                                    logItem.type === "normal" && (typeof logItem.content == "string" || typeof logItem.content == "boolean" || typeof logItem.content == "number" || !logItem.content)
+                                                        ? "pl-1  text-emerald-500 break-words "
+                                                        : logItem.type === "warning" ? "pl-1 bg-warning text-warning-content py-1  "
+                                                            : "pl-1 bg-error text-error-content py-1  "
 
-                                                   }>{
-                                                   typeof logItem.content == "string" || typeof logItem.content == "boolean"|| typeof logItem.content == "number"|| !logItem.content
-                                                       ?  index+ "> "+String(logItem.content)
-                                                   :  index+ "> Print accepts: string, number, boolean, null, undefiend, or arrays from those types, or nested arrays from those types"
-                                               }</code>
-                                            </pre>
+                                                }>{
+                                                    typeof logItem.content == "string" || typeof logItem.content == "boolean" || typeof logItem.content == "number" || !logItem.content
+                                                        ? index + "> " + String(logItem.content)
+                                                        : index + "> Print accepts: string, number, boolean, null, undefiend, or arrays from those types, or nested arrays from those types"
+                                                }</code>
+                                        </pre>
 
 
 
@@ -177,11 +177,11 @@ function CodeOperations() {
                                 })
                             )
                     }
-                    </div>
                 </div>
             </div>
+        </div>
 
-        );
+    );
 }
 
 export default CodeOperations;
